f2bd04f55983469225ffdc6747d8e777c090ee21
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-06-27 10:52:16 +0600

    complex view fxml and controler added

---
+++ src/main/java/com/example/complexnumber/ComplexViewControler.java
@@ -0,0 +1,30 @@
+package com.example.complexnumber;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ComplexViewControler {
+
+    @FXML private TextField firstimgcom;
+
+    @FXML private TextField firstrealcomp;
+
+    @FXML private TextField secimgcom;
+
+    @FXML private TextField secrealcom;
+
+    @FXML private Label showresult;
+
+    @FXML
+    void addcomplex(ActionEvent event) {
+
+    }
+
+    @FXML
+    void subcomplex(ActionEvent event) {
+
+    }
+
+}
